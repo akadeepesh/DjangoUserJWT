77b4db53077061c3beeb2670a53c193e7817772f
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-10 01:40:27 +0530

    railway config

--- djangoauthjwt/djangoauthjwt/settings.py
+++ djangoauthjwt/djangoauthjwt/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
