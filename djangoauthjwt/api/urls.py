b55b1402b1c5a2a0a694aaef78d9dce1af48aa8a
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-10 01:11:28 +0530

    Connected postgresql from vercel and got ready to deploy on vercel

--- djangoauthjwt/api/urls.py
+++ djangoauthjwt/api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("tokenize/", views.TokenizeSentence.as_view(), name="TokenizeSentence"),
     path("remove_stop_words/", views.RemoveStopwords.as_view(), name="RemoveStopwords"),
-    path("noise-reduction/", views.NoiseReduction.as_view(), name="NoiseReduction"),
+    # path("noise-reduction/", views.NoiseReduction.as_view(), name="NoiseReduction"),
     path('stemlem/', views.StemLemWords.as_view(), name='Stemming Lemmatization'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- djangoauthjwt/djangoauthjwt/settings.py
+++ djangoauthjwt/djangoauthjwt/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -92,9 +92,14 @@ WSGI_APPLICATION = "djangoauthjwt.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'ATOMIC_REQUESTS': True,  # Required for transactions
     }
 }
 
